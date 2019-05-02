b5a7d1528c7548640024bdb7392eb3430af7776d
Gabriel Helko Meyer <gabriel.meyer@softin.com.br>
2019-05-02 14:35:52 -0300

    Iniciado criacao de testes de Integracao

--- TestAutomationApi.NetCore2/UnitTest1.cs
+++ TestAutomationApi.NetCore2/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+using RestSharp;
 using System;
 using Xunit;
 
@@ -5,9 +7,19 @@ namespace TestAutomationApi.NetCore2
 {
     public class UnitTest1
     {
+        private IRestResponse GetCEP(object CEP)
+        {
+            var client = new RestClient("https://viacep.com.br/ws/" + CEP + "/json/");
+            var RSrequest = new RestRequest(Method.GET) { RequestFormat = DataFormat.Json };
+
+            return client.Execute(RSrequest);
+        }
         [Fact]
         public void Test1()
         {
+            var response = GetCEP("89221515");
+            //response.StatusCode.Should().Be(200);
+            Assert.Contains("Rua Iririú", response.Content);
 
         }
     }
