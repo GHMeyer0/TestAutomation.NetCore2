53eb5c61921d6c380833f8f778293f0dc60d1f9d
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-05-01 21:35:46 -0300

    Add project files.

---
+++ TestAutomation-.NetCore2/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestAutomation_.NetCore2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
